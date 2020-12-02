3061235d23d1858c6f118e98608d028eef651ac9
connorhw <connorwong93@gmail.com>
2020-12-01 22:55:39 -0500

    added full stack compatibility, with one-to-many table relationships in the db with all the appropriate test.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const { json } = require('express')
 const articlesRouter = require('./articles/articles-router')
+const usersRouter = require('./users/users-router')
+const commentsRouter = require('./comments/comments-router')
 
 const app = express()
 
@@ -19,6 +21,8 @@ app.use(helmet())
 app.use(cors())
 
 app.use('/api/articles', articlesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/comments', commentsRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
